52ce38a839349af96d945501ce555d614eb8d860
pop1234o <859405648@qq.com>
2019-07-26 21:12:28 +0800

    feat:创建了几个模块

---
+++ lib_common/src/main/java/com/liyafeng/lib_common/AppUtil.kt
@@ -0,0 +1,5 @@
+package com.liyafeng.lib_common
+
+class AppUtil{
+
+}
---
+++ module_convert/src/main/java/debug/DebugConvertMainActivity.kt
@@ -0,0 +1,13 @@
+package debug
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.liyafeng.module_convert.R
+
+class DebugConvertMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debug_convert_main)
+    }
+}
